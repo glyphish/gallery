9721eae6e4ec60a359063ca43274c0cfb37752c2
Jörgen Isaksson <jogga@bitfield.se>
2014-03-16 13:48:37 +0100

    Initial Commit

---
+++ Icon Gallery/BFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BFAppDelegate.h
+//  Icon Gallery
+//
+//  Created by Jörgen Isaksson on 2014-03-16.
+//  Copyright (c) 2014 Bitfield AB. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BFAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Icon Gallery/BFAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  BFAppDelegate.m
+//  Icon Gallery
+//
+//  Created by Jörgen Isaksson on 2014-03-16.
+//  Copyright (c) 2014 Bitfield AB. All rights reserved.
+//
+
+#import "BFAppDelegate.h"
+
+@implementation BFAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
